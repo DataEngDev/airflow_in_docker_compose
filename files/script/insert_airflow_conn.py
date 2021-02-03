bd66b82bd8d393e0343830443815a75aa6818b17
yennan.liu <yennan.liu@rtwpc30008.intra.rakuten.co.jp>
2021-02-03 16:22:40 +0800

    update files/script/insert_airflow_conn.py, creds.yml.dev

--- files/script/insert_airflow_conn.py
+++ files/script/insert_airflow_conn.py
@@ -10,6 +10,13 @@ with open('.creds.yml') as f:
 LOCAL_SSH_USER = config['ssh_local']['user']
 LOCAL_SSH_USER_PASSWORD = config['ssh_local']['password']
 
+SERVER1_USER = config['server_1']['user']
+SERVER1_PASSWORD = config['server_1']['password']
+
+
+def clean_conn_table(cursor):
+    sql = """truncate public.connection"""
+    cursor.execute(sql)
 
 def insert_local_ssh_conn(cursor):
     sql = """
@@ -23,6 +30,17 @@ def insert_local_ssh_conn(cursor):
     cursor.execute(sql)
 
 
+def insert_server1_conn(cursor):
+    sql = """
+        INSERT INTO public."connection"
+        (conn_id, conn_type, host, "schema", login, password, port, extra, is_encrypted, is_extra_encrypted)
+        VALUES('server1_default', 'ssh', 'xxx.xxx.xxx', '', '{SERVER1_USER}', '{SERVER1_PASSWORD}', 22, '', false, false);
+        """.format(
+            SERVER1_USER=SERVER1_USER,
+            SERVER1_PASSWORD=SERVER1_PASSWORD
+            )
+    cursor.execute(sql)
+
 def main():
     # config 
     ### host='postgres' for the postgres in docker-compose
@@ -30,10 +48,18 @@ def main():
     conn_string = "host='postgres' dbname='airflow' user='airflow' password='airflow'"
     conn = psycopg2.connect(conn_string)
     conn.autocommit = True
+    
     try: 
         cursor = conn.cursor()
+
+        print('clean connection table')
+        clean_conn_table(cursor)
+
         print('Inserting local ssh conn')
         insert_local_ssh_conn(cursor)
+
+        print('Inserting local ssh conn')
+        insert_server1_conn(cursor)
 
     except Exception as e:
         print ('Insert credentials failed.. ')
